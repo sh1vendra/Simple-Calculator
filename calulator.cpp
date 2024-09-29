e5bb6f6c90cfad3f9329760e3b9fa198fba071a9
sh1vendra <shivendra@txstate.edu>
2024-09-28 20:27:51 -0500

    Add multiplication  function

--- calulator.cpp
+++ calulator.cpp
@@ -9,3 +9,7 @@ void add(double num1, double num2) {
 void subtract(double num1, double num2) {
     cout << "Result: " << num1 - num2 << endl;
 }
+
+void multiply(double num1, double num2) {
+    cout << "Result: " << num1 * num2 << endl;
+}
