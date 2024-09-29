77fe8a8a308e4cc0e0fd3e33995d76e994f78f15
sh1vendra <shivendra@txstate.edu>
2024-09-28 21:20:52 -0500

    Formatting Document

--- calulator.cpp
+++ calulator.cpp
@@ -2,27 +2,35 @@
 
 using namespace std;
 
-void add(double num1, double num2) {
+void add(double num1, double num2)
+{
     cout << "Result: " << num1 + num2 << endl;
 }
 
-void subtract(double num1, double num2) {
+void subtract(double num1, double num2)
+{
     cout << "Result: " << num1 - num2 << endl;
 }
 
-void multiply(double num1, double num2) {
+void multiply(double num1, double num2)
+{
     cout << "Result: " << num1 * num2 << endl;
 }
 
-void divide(double num1, double num2) {
-    if(num2 != 0) {
+void divide(double num1, double num2)
+{
+    if (num2 != 0)
+    {
         cout << "Result: " << num1 / num2 << endl;
-    } else {
+    }
+    else
+    {
         cout << "Error: Division by zero!" << endl;
     }
 }
 
-int main() {
+int main()
+{
     double num1, num2;
     char operation;
     bool validOperation = false;
@@ -32,32 +40,33 @@ int main() {
     cout << "Enter second number: ";
     cin >> num2;
 
-    while (!validOperation) {
+    while (!validOperation)
+    {
         cout << "Enter operation (+, -, *, /): ";
         cin >> operation;
 
-        switch(operation) {
-            case '+':
-                add(num1, num2);
-                validOperation = true;
-                break;
-            case '-':
-                subtract(num1, num2);
-                validOperation = true;
-                break;
-            case '*':
-                multiply(num1, num2);
-                validOperation = true;
-                break;
-            case '/':
-                divide(num1, num2);
-                validOperation = true;
-                break;
-            default:
-                cout << "Invalid operation! Please try again." << endl;
+        switch (operation)
+        {
+        case '+':
+            add(num1, num2);
+            validOperation = true;
+            break;
+        case '-':
+            subtract(num1, num2);
+            validOperation = true;
+            break;
+        case '*':
+            multiply(num1, num2);
+            validOperation = true;
+            break;
+        case '/':
+            divide(num1, num2);
+            validOperation = true;
+            break;
+        default:
+            cout << "Invalid operation! Please try again." << endl;
         }
     }
 
     return 0;
 }
-
