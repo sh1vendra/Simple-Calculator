477c7819302b1e37fc1d18ca98704feba233e66e
sh1vendra <shivendra@txstate.edu>
2024-09-28 20:30:45 -0500

    Error handling for division by 0

--- calulator.cpp
+++ calulator.cpp
@@ -18,3 +18,7 @@ void divide(double num1, double num2) {
     if(num2 != 0) {
         cout << "Result: " << num1 / num2 << endl;
     }
+    else {
+        cout << "Error: Division by zero!" << endl;
+    }
+}
